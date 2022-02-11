b34f13635d397b1c7b34e2658b7d523bd941d132
debbyne <kinyadebbyne@gmail.com>
2022-02-11 07:53:02 +0300

    update giphy spec file

--- src/app/giphy.spec.ts
+++ src/app/giphy.spec.ts
@@ -2,6 +2,6 @@ import { Giphy } from './giphy';
 
 describe('Giphy', () => {
   it('should create an instance', () => {
-    expect(new Giphy()).toBeTruthy();
+    expect(new Giphy([])).toBeTruthy();
   });
 });
